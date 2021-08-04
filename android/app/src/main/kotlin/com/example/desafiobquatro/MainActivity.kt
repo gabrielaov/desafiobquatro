07756954784fd4329949145cf1889fb765662869
OliveiraViana <Gabriela_Viana@Dellteam.com>
2021-08-04 11:29:18 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/desafiobquatro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desafiobquatro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Adder.dart
@@ -0,0 +1,8 @@
+class Adder{
+  int numero1 = 0;
+  int numero2 = 0;
+
+  soma() {
+    numero1 + numero2;
+  }
+}
